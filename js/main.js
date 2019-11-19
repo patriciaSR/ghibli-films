5ad300f2c4925dca8ae5cac1bde6fda6b181ab01
patriciaSR <patricia.suarez.psr@gmail.com>
2019-11-19 13:37:43 +0100

    add setTimeout and loading function

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const ENDPOINT = 'https://ghibliapi.herokuapp.com/films';
 
 let filmsData = [];
 
-// //get films from API
+// //get films from API with fetch
 // fetch(ENDPOINT)
 // .then(res => res.json())
 // .then(data => {
@@ -16,49 +16,6 @@ let filmsData = [];
 
 //   return printList(filmsData)})
 // .catch(error => console.log(error))
-
-// function printList(films) {
-//   list.innerHTML= '';
-
-//   const newFilms = films.map (film => {
-//     const newLi = document.createElement('li');
-//     const newText = document.createTextNode(film.title);
-//     newLi.appendChild(newText);
-//     return newLi;
-//   })
-  
-//   for (const film of newFilms) {
-//     list.appendChild(film);
-//   }
-// };
-
-//filter films list
-function filterFilms() {
-const query = queryInput.value.toUpperCase();
-const filteredFilms = filmsData.filter(film => film.title.toUpperCase().includes(query));
-
-return printList(filteredFilms);
-}
-
-searchBtn.addEventListener('click', filterFilms);
-
-
-//get films from API
-
-sendRequest();
-
-function sendRequest() {
-  setTimeout(function() {
-    fetch(ENDPOINT)
-  .then(res => res.json())
-  .then(data => {
-    filmsData = data;
-  
-    return printList(filmsData)})
-  .catch(error => console.log(error))
-  }, 5000);
-}
-
 
 function printList(films) {
   list.innerHTML= '';
@@ -74,3 +31,40 @@ function printList(films) {
     list.appendChild(film);
   }
 };
+
+//filter films list
+function filterFilms() {
+const query = queryInput.value.toUpperCase();
+const filteredFilms = filmsData.filter(film => film.title.toUpperCase().includes(query));
+
+return printList(filteredFilms);
+}
+
+searchBtn.addEventListener('click', filterFilms);
+
+
+//get films from API with setTimeout and loading msg
+loadingMsg();
+sendRequest();
+
+function sendRequest() {
+  setTimeout(function() {
+    fetch(ENDPOINT)
+    .then(res => res.json())
+    .then(data => {
+      filmsData = data;
+    
+      return printList(filmsData)})
+    .catch(error => console.log(error))
+  }, 5000);
+}
+
+function loadingMsg() {
+  const newP = document.createElement('p');
+  const newText = document.createTextNode('Loading...');
+  newP.add
+  newP.appendChild(newText);
+  return list.appendChild(newP);
+}
+
+
