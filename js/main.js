37c37caf23372e6a9c9c7b35ac1de4caa9e4c956
patriciaSR <patricia.suarez.psr@gmail.com>
2019-11-19 13:28:36 +0100

    add fetch method working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,76 @@
 'use strict';
 
-console.log("running!");
+const list = document.querySelector('.films__list');
+const searchBtn = document.querySelector('.filter__button');
+const queryInput = document.querySelector('.filter__input');
+
+const ENDPOINT = 'https://ghibliapi.herokuapp.com/films';
+
+let filmsData = [];
+
+// //get films from API
+// fetch(ENDPOINT)
+// .then(res => res.json())
+// .then(data => {
+//   filmsData = data;
+
+//   return printList(filmsData)})
+// .catch(error => console.log(error))
+
+// function printList(films) {
+//   list.innerHTML= '';
+
+//   const newFilms = films.map (film => {
+//     const newLi = document.createElement('li');
+//     const newText = document.createTextNode(film.title);
+//     newLi.appendChild(newText);
+//     return newLi;
+//   })
+  
+//   for (const film of newFilms) {
+//     list.appendChild(film);
+//   }
+// };
+
+//filter films list
+function filterFilms() {
+const query = queryInput.value.toUpperCase();
+const filteredFilms = filmsData.filter(film => film.title.toUpperCase().includes(query));
+
+return printList(filteredFilms);
+}
+
+searchBtn.addEventListener('click', filterFilms);
+
+
+//get films from API
+
+sendRequest();
+
+function sendRequest() {
+  setTimeout(function() {
+    fetch(ENDPOINT)
+  .then(res => res.json())
+  .then(data => {
+    filmsData = data;
+  
+    return printList(filmsData)})
+  .catch(error => console.log(error))
+  }, 5000);
+}
+
+
+function printList(films) {
+  list.innerHTML= '';
+
+  const newFilms = films.map (film => {
+    const newLi = document.createElement('li');
+    const newText = document.createTextNode(film.title);
+    newLi.appendChild(newText);
+    return newLi;
+  })
+  
+  for (const film of newFilms) {
+    list.appendChild(film);
+  }
+};
