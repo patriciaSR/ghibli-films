3574a897febaf1cb4fda14e1de4f7cccc55b96d9
patriciaSR <patricia.suarez.psr@gmail.com>
2019-11-24 14:27:15 +0100

    create 2 test for createTag function

---
+++ test/createTags.test.js
@@ -0,0 +1,29 @@
+import { createTag } from '../js/createTags.js';
+
+describe('createTags module', () => {
+  describe('createTag function', () =>{
+    test('it creates a <div>', () => {
+      const tagName = 'div';
+      const text = 'foo';
+
+      const result = createTag(tagName, text);
+
+      expect(result.tagName).toBe(tagName.toUpperCase());
+      expect(result.textContent).toBe(text);
+    });
+
+    test('it creates a <p> with a class', () => {
+      const tagName = 'p';
+      const text = 'foo';
+      const className = 'foo';
+
+      const result = createTag(tagName, text, className);
+
+      expect(result.tagName).toBe(tagName.toUpperCase());
+      expect(result.textContent).toBe(text);
+      expect(result.classList).toContain(className);
+      // Otra opción:
+      // expect(result.classList.contains(className)).toBeTruthy();
+    });
+  });
+});
